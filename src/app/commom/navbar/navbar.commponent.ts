788b661408052784eae7384861981c25006822f1
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2022-01-09 09:16:00 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/commom/navbar/navbar.commponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.commponent.html',
+  styleUrls: ['./navbar.commponent.scss']
+})
+export class NavbarComponent {
+}
+
+
