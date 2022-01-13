43e1f97c83016735f458a0a25b5463e751507062
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2022-01-13 22:38:39 +0900

    mongoDB接続不可　No89

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
-// Using Node.js `require()`
 const mongoose = require('mongoose');
 const config = require('./config/dev')
+const FakeDb = require('./fake-db')
+console.log('config.DB_URI=>' + config.DB_URI);
 
-mongoose.connect(config.DB_URI);
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://taka:<password>@cluster0.n3phz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+mongoose.connect(config.DB_URI)
+.then(
+    () => {
+        console.log('connect OK');
+        const fakeDb = new FakeDb()
+        fakeDb.seedDb()
+    }
+)
+console.log('connect after')
 
 //mongodb+srv://taka:<password>@cluster0.n3phz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const app = express()
--- server/model/product.js
+++ server/model/product.js
@@ -1,9 +1,21 @@
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+// const ObjectId = Schema.ObjectId;
 
 const ProductSchema = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
+  coverImage: String,
+  name: { type:String, require: true ,max:[60,'最大60文字までです']},
+  price: Number,
+  description: String,
+  heding1: String,
+  heding2: String,
+  heding3: String,
+  hedingtext1: String,
+  hedingtext2: String,
+  hedingtext3: String,
+
 });
+
+module.exports = mongoose.model('Product',ProductSchema)
+
