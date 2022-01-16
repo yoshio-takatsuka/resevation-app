2e09329ae8a89aaa343fa10418c35e03bb98d9f6
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2022-01-16 10:03:45 +0900

    No100完了

--- server/route/products.js
+++ server/route/products.js
@@ -4,16 +4,18 @@ const Product = require('../model/product')
 
 router.get('',function(req,res){
     Product.find({},function(err,foundProducts){
-        res.json(foundProducts)
+        return res.json(foundProducts)
     })
 
 })
 
-
 router.get('/:productId',function(req,res){
     const productId = req.params.productId
     Product.findById(productId,function(err,foundProduct){
-        res.json(foundProduct)
+       if (err){
+           return res.status(422).send({errors: [{title: 'aproduct error',detail: 'Product not found'}]})
+       }
+       return res.json(foundProduct)
     })
 
 })
